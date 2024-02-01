3771368f74009a3d95152a7e72ad600ac4230c50
Artur-Lambo <lamboartur94@gmail.com>
2024-02-01 22:30:12 +0100

    Création de fichiers js pour la connexion et l'authentification vers l'api web Création d'un nouveau formulaire d'authentification et création des roles admin et utilisateur Réorganisation du code et création d'un fichier de configurations

---
+++ CONTROLLER/auth.js
@@ -0,0 +1,14 @@
+const config = require('../config');
+
+// Utiliser la valeur apiUrl
+console.log('La valeur de apiUrl est :', config.apiUrl);
+
+
+// var url_api=config.apiUrl();
+// config.apiUrl.require()
+
+// async function useAPi(url){
+//     console.log(url);
+
+// }
+// useAPi(url_api);
---
+++ CONTROLLER/connexion.js
@@ -0,0 +1,46 @@
+// Inclure le contenu de auth.js
+const script2 = require('./auth.js');
+
+function authentification() {
+    var login = document.getElementById('login').value;
+    var password = document.getElementById('mdp').value;
+    var role = document.getElementById('role').value;
+    var messageElement = document.getElementById('message');
+
+    if (login === "" || password === "") {
+        return false;
+    }
+    if (login === "admin" && password === "admin") {
+        if (role === "ADMINISTRATEUR") {
+            // Mettre en place un temps de chargement de page avec une librairie
+            messageElement.textContent = "AUTHENTIFICATION ACCEPTEE :)";
+            setTimeout(function () {
+                messageElement.textContent = "";
+                window.location.href = '../Menu.html';
+            }, 10000);
+            return true;
+        } else if (role === "UTILISATEUR") {
+            // On affiche une page contenant la liste des taches pour cette utilisateur
+            alert('bien')
+            return true;
+        } else {
+            messageElement.textContent = "Cette utilisateur est valide mais ne dispose pas du bon role !!";
+            setTimeout(function () {
+                messageElement.textContent = "";
+            }, 10000);
+            return false;
+        }
+    } else {
+        messageElement.textContent = "Identifiant ou mot de passe incorrect !!";
+        setTimeout(function () {
+            messageElement.textContent = "";
+        }, 10000);
+        return false;
+    }
+    role = '';
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Appeler authentification() lorsque le document est complètement chargé
+    authentification();
+});
+
