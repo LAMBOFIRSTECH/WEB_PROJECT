3df1a6846043943361e61c79c4c8b9bd3cb153e9
Artur-Lambo <lamboartur94@gmail.com>
2024-02-03 18:32:01 +0100

    ajout

--- CONTROLLER/auth.mjs
+++ CONTROLLER/auth.mjs
@@ -1,5 +1,5 @@
 import { get } from 'http';
-import config, { taskEndpoints } from '../config.mjs';
+import config, { taskEndpoints,userEndpoints } from '../config.mjs';
 import fetch from 'node-fetch'
 
 //var idTask = document.getElementById('id_task').value;
@@ -7,10 +7,15 @@ var id_task = 0
 function getAllEndpoint() {
     var sourceUrl = ""
     const taskUri = {}
+    var urls=[]
+    var url_all_tasks = ""
+    var url_one_task = ""
+    let found = false
+
     const regex = /^(.*)\/index\.html$/;
     const correspondre = config.apiUrl.match(regex)
     if (!correspondre) {
-        console.log("La regex ne fonctionne pas")
+        console.log("La regex ne matche pas")
     }
     else {
         sourceUrl = correspondre[1]
@@ -18,28 +23,34 @@ function getAllEndpoint() {
         var one_task = sourceUrl + taskEndpoints.select(id_task)
         var delete_one_task = sourceUrl + taskEndpoints.delete(id_task)
         const taskUri = {
-            all_taks: `${tasks}`,
+            all_tasks: `${tasks}`,
             one_task: `${one_task}`,
             delete_task: `${delete_one_task}`
         };
-        return taskUri
+       
+        Object.keys(taskUri).forEach(key => {
+            if (key === 'all_tasks') {
+                url_all_tasks = taskUri[key];
+                found = true
+            }
+            if (key === 'one_task') {
+                url_one_task = taskUri[key];
+                found = true
+            }
+            
+        });
+        var users_url=sourceUrl + userEndpoints.get
+        urls.push(url_all_tasks)
+        urls.push(url_one_task)
+        urls.push(users_url)
+        return urls
     }
 }
 
-async function getTask() {
-    var url = ""
-    var taskUri = getAllEndpoint()
-    let found = false
-    Object.keys(taskUri).forEach(key => {
-        if (key === 'all_taks') {
-            url = taskUri[key];
-            found = true
-        }
-
-    });
-    
+async function getTasks() {
+    var urls=getAllEndpoint()
     try {
-        const res = await fetch(url)
+        const res = await fetch(urls[0])
         if (!res.ok) {
             throw new Error(`HTTP error! Status: ${res.status}`);
         }
@@ -53,4 +64,26 @@ async function getTask() {
     }
 
 }
-getTask()
+async function getUsers() {
+    var urls=getAllEndpoint()
+    try {
+        const res = await fetch(urls[2])
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        var data = await res.json()
+        console.log(data)
+
+    } catch
+    (error) {
+        console.error('Error during fetch:', error.message);
+
+    }
+
+}
+function main() {
+   //getTasks()
+   getUsers()
+
+}
+main()
